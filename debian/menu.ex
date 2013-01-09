c144677fb0a2060c4bd6048426d172b6c993eeb5
Devon Kearns <dookie@kali.org>
2013-01-08 19:05:27 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ncrack):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ncrack" command="/usr/bin/ncrack"
--- debian/ncrack.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ncrack package
-#
-0 4	* * *	root	[ -x /usr/bin/ncrack_maintenance ] && /usr/bin/ncrack_maintenance
